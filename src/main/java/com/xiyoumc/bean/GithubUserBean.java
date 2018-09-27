b9b1bf6a58bdf5fabb68960f4306527c1c2bc325
xiyouMc <xiyoumc.dev@gmail.com>
2018-09-27 20:20:18 +0800

    init :Support Login

---
+++ src/main/java/com/xiyoumc/bean/GithubUserBean.java
@@ -0,0 +1,32 @@
+package com.xiyoumc.bean;
+
+public class GithubUserBean {
+
+  private String user;
+  private String avatar;
+  private String fuck_username;
+
+  public String getUser() {
+    return user;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+  public String getAvatar() {
+    return avatar;
+  }
+
+  public void setAvatar(String avatar) {
+    this.avatar = avatar;
+  }
+
+  public String getFuck_username() {
+    return fuck_username;
+  }
+
+  public void setFuck_username(String fuck_username) {
+    this.fuck_username = fuck_username;
+  }
+}
---
+++ src/main/java/com/xiyoumc/web/GreetingController.java
@@ -0,0 +1,29 @@
+package com.xiyoumc.web;
+
+import com.xiyoumc.model.Greeting;
+import com.xiyoumc.service.HelloWorldService;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+  private final AtomicInteger counter = new AtomicInteger();
+  private static final String TEMPLATE = "Hello, %s!";
+  @Autowired
+  private HelloWorldService helloWorldService;
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
+  }
+
+  @RequestMapping("/")
+  public String helloWorld() {
+    return this.helloWorldService.getHelloMessage();
+  }
+
+}
