405d3f13553f349d5d631f75782eb3ca210a67af
xiyouMc <xiyoumc.dev@gmail.com>
2018-09-30 14:30:56 +0800

    add Tomcat War

--- src/main/java/com/xiyoumc/controller/InsController.java
+++ src/main/java/com/xiyoumc/controller/InsController.java
@@ -1,34 +1,37 @@
 package com.xiyoumc.controller;
 
-import com.xiyoumc.bean.InsPicBean;
 import com.xiyoumc.dao.InsCacheDao;
 import com.xiyoumc.entity.InsCache;
-import com.xiyoumc.http.RestTemplateUtils;
-import com.xiyoumc.util.JacksonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class InsController {
 
   private static final Logger logger = LoggerFactory.getLogger(InsController.class);
-
+  private static final int REDIS_GET_TIMEOUT = 8 * 1000;
   @Autowired
   private InsCacheDao insCacheDao;
 
-  @RequestMapping("/q")
-  public InsPicBean requestPicInfoByURL(@RequestParam("url") String url) {
+  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
 
-    if (url.contains("instagram.com") && url.contains("?")) {
-      String[] insUrls = url.split("[?]");
-      url = insUrls[0] + "?__a=1";
+  @RequestMapping(value = "/q", produces = "application/json;charset=UTF-8")
+  public String requestPicInfoByURL(@RequestParam("url") String url) {
+
+    if (url.contains("instagram.com")) {
+      if (url.contains("?")) {
+        String[] insUrls = url.split("[?]");
+        url = insUrls[0] + "?__a=1";
+      } else {
+        url += "?__a=1";
+      }
+
     }
     System.out.println("insUrl:" + url);
 
@@ -37,11 +40,34 @@ public class InsController {
      * 2、如果 Redis 有数据则直接返回 ，没有的话 将数据插入 Redis 分布式请求数据。
      * 3、在等待 Redis 的时候 需要 做时间控制，暂定 8 s 等待时间
      */
+    /**
+     * 暂时直接操作 Redis 吧， 不使用 DB
+     */
 
-    RestTemplate restTemplate = RestTemplateUtils.newRestTemplate();
-    ResponseEntity<String> responseEntity = restTemplate
-        .exchange(url, HttpMethod.GET, null, String.class);
-    return JacksonUtil.readValue(responseEntity.getBody(), InsPicBean.class);
+    //1、查询 Redis
+    String redisData = stringRedisTemplate.opsForValue().get(url);
+    if (redisData != null && !redisData.isEmpty()) {
+      System.out.println(redisData);
+    } else {
+      stringRedisTemplate.opsForList().rightPush("ins", url);
+      long startTime = System.currentTimeMillis();
+      long endTime = System.currentTimeMillis();
+      while ((endTime - startTime) < REDIS_GET_TIMEOUT) {
+        redisData = stringRedisTemplate.opsForValue().get(url);
+        if (redisData != null && !redisData.isEmpty()) {
+          break;
+        }
+        endTime = System.currentTimeMillis();
+      }
+    }
+//    ObjectMapper mapper = new ObjectMapper();
+//    return mapper.readValue(redisData,Map.class);
+    return redisData;
+
+//    RestTemplate restTemplate = RestTemplateUtils.newRestTemplate();
+//    ResponseEntity<String> responseEntity = restTemplate
+//        .exchange(url, HttpMethod.GET, null, String.class);
+//    return JacksonUtil.readValue(responseEntity.getBody(), InsPicBean.class);
   }
 
   @RequestMapping("/testAddIns")
